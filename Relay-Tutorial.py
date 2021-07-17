ab4cb62ea7a84ad5f134d8628633830bef24bd1e
rijulchaturvedi <66110217+rijulchaturvedi@users.noreply.github.com>
2021-07-17 12:49:13 +0530

    Update Relay-Tutorial.py

--- Relay-Tutorial.py
+++ Relay-Tutorial.py
@@ -1,27 +1,56 @@
+from gpiozero import LED
+from gpiozero import MotionSensor
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
+pir = MotionSensor(17) # PIR Motion Sensor
+GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
 
 # Relay 1
 GPIO.setup(21, GPIO.OUT)
 # Relay 2
-GPIO.setup(26, GPIO.OUT)
+# GPIO.setup(26, GPIO.OUT)
 
 try:
     while True:
+        pir.wait_for_motion()
+        print("Motion Detected")
         GPIO.output(21, GPIO.HIGH)
         print('Relay 1 ON')
         time.sleep(1)
-        GPIO.output(26, GPIO.HIGH)
-        print('Relay 2 ON')
-        time.sleep(1)
+        # GPIO.output(26, GPIO.HIGH)
+        # print('Relay 2 ON')
+        # time.sleep(1)
         GPIO.output(21, GPIO.LOW)
         print('Relay 1 OFF')
-        time.sleep(1)
-        GPIO.output(26, GPIO.LOW)
-        print('Relay 2 OFF')
-        time.sleep(1)
+        # time.sleep(1)
+        # GPIO.output(26, GPIO.LOW)
+        # print('Relay 2 OFF')
+        # time.sleep(1)
         
 finally:
     GPIO.cleanup()
+    
+    
+
+
+
+ 
+# RELAIS_1_GPIO = 21
+# GPIO.setup(RELAIS_1_GPIO, GPIO.OUT) # GPIO Assign mode
+# GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # out
+# GPIO.output(RELAIS_1_GPIO, GPIO.HIGH) # on
+
+
+# led = LED(21)
+
+# led.off()
+
+# while True:
+       
+        # GPIO.output(RELAIS_1_GPIO, GPIO.HIGH)
+        # time.sleep(2)
+        # pir.wait_for_motion()
+        # GPIO.output(RELAIS_1_GPIO, GPIO.LOW)
+        # print("Motion Stopped")
+        # time.sleep(2)
